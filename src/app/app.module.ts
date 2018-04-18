597f767e496942de70784728b8dd66ad5e35114b
iimToast <kevin.brusson@gmail.com>
2018-04-18 16:49:04 +0200

    Ajout de page, css manquants, liens faits pour les trois premières pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { FavorisPage } from '../pages/favoris/favoris';
+import { MonComptePage } from '../pages/mon-compte/mon-compte';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,7 +19,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    FavorisPage,
+    MonComptePage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +33,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    FavorisPage,
+    MonComptePage
   ],
   providers: [
     StatusBar,
